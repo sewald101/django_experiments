e64adc3ae567ec9e5592ccabd50d878e2fc2112a
sewald101 <sewald101@gmail.com>
2020-05-08 08:34:01 -0700

    Installed jupyter notebook python extension

--- steves_twitter_apps/geohash/models.py
+++ steves_twitter_apps/geohash/models.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 
 # Create your models here.
 class Woeids(models.Model):
-    country = models.CharField(max_length=200)
-    countryCode = models.CharField(max_length=50, null=True)
-    name = models.CharField(max_length=200)
-    parentid = models.IntegerField(default=0, null=True)
-    placeType = models.CharField(max_length=200)
-    url = models.CharField(max_length=500)
-    woeid = models.IntegerField(default=0)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    countryCode = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    parentid = models.IntegerField(default=0, null=True, blank=True)
+    placeType = models.CharField(max_length=200, null=True, blank=True)
+    url = models.CharField(max_length=500, null=True, blank=True)
+    woeid = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return '%s %s %s %s %s %s %s' % (
