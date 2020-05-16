fc5eb5f56e7e4eeb970afc09da4da0e7d7114946
sewald101 <sewald101@gmail.com>
2020-05-16 13:55:12 -0700

    replace views and URLconfs with Django generic views

--- steves_twitter_apps/polls/urls.py
+++ steves_twitter_apps/polls/urls.py
@@ -1,19 +1,29 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 app_name = 'polls'
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+"""  OLD CODE FIRST PART OF TUTORIAL
+urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.index_2, name='index'),
     
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results_2, name='results'),
     
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+"""
--- steves_twitter_apps/polls/views.py
+++ steves_twitter_apps/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
 
 import json
 import numpy as np
@@ -12,26 +13,20 @@ import os
 
 from .models import Question, Choice
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    title = ("Polls app -- Django official tutorial at: "
-             "https://docs.djangoproject.com/en/3.0/"
-    )
-    invite = "Take a poll!"
-    context = {'header':title, 
-               'invite':invite,
-               'questions':latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index_2.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.all()
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'    
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results_2.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -51,3 +46,24 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
+""" OLD CODE EARLY IN TUTORIAL
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    title = ("Polls app -- Django official tutorial at: "
+             "https://docs.djangoproject.com/en/3.0/"
+    )
+    invite = "Take a poll!"
+    context = {'header':title, 
+               'invite':invite,
+               'questions':latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+"""
