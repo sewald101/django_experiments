5ff13675f512d09e8095d626c5cba7139c41a845
sewald101 <sewald101@gmail.com>
2020-05-27 07:25:02 -0700

    rename URLconf, clean up views, add header to result.html

--- steves_twitter_apps/geohash/urls.py
+++ steves_twitter_apps/geohash/urls.py
@@ -6,7 +6,8 @@ app_name = 'geohash'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('CountrySelect/', views.tophash_02, name='hackin'),
+    path('CountrySelect/', views.tophash_by_country, name='ByCountry'),
+    path('MetroSelect/', views.tophash_by_metro, name='ByMetro'),
     path('m/<name>/', views.tophash, name='tophash_name'),
     path('m/<country>/<name>/', views.tophash, name='tophash'),
     path('m/<country>/', views.tophash, name='tophash_country'),
--- steves_twitter_apps/geohash/views.py
+++ steves_twitter_apps/geohash/views.py
@@ -29,7 +29,7 @@ def index(request):
     return render(request, 'geohash/index.html', context)
 
 def tophash(request, name='', country=''):
-    """ This view returns results from country and/or name parameters entered manually
+    """ Return results from country and/or name parameters entered manually
     via the address bar of the browser with the '/m/' pattern. 
         E.g. localhost:8000/geohash/m/france/paris
     """
@@ -57,25 +57,15 @@ def tophash(request, name='', country=''):
     top_hashes = trending_by_geo(woeid=woeid)
     return render(request, "geohash/result.html", {'d': top_hashes})
 
-def tophash_02(request, name='', country=''):
-    """This view returns results from country entered via the dropdown form on the homepage.
+def tophash_by_country(request):
+    """Return results from country entered via the dropdown form on the homepage.
     """
     print("LOG 2.0 - request object: {}".format(request))
     print("LOG 2.1 - request.GET object: {}".format(request.GET))
     print("LOG 2.2 - request.GET.country object: {}".format(request.GET['country']))
     country = request.GET['country'].title()
-    name = name.title()
-    if not country: # For worldwide
-        querySet = (
-        Woeids.objects.filter(name = name).values('woeid')
-        )
-    elif not name: # For countries at country grain
-        querySet = (
+    querySet = (
         Woeids.objects.filter(name = country).filter(country = country).values('woeid')
-        )
-    else: # For country, city
-        querySet = (
-        Woeids.objects.filter(name = name).filter(country = country).values('woeid')
         )
     print("LOG 2.3 - This is the country being passed to the Woeids query: {}"
           .format(country))
@@ -83,5 +73,8 @@ def tophash_02(request, name='', country=''):
           .format(querySet))
     woeid = querySet[0]['woeid']
     top_hashes = trending_by_geo(woeid=woeid)
-    return render(request, "geohash/result.html", {'d': top_hashes})
+    context = {'country': country, 'd': top_hashes}
+    return render(request, "geohash/result.html", context)
 
+def tophash_by_metro(request):
+    pass
