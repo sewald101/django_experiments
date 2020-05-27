1d00bc613d407a222b5df582fb74c93809f6dd35
sewald101 <sewald101@gmail.com>
2020-05-26 19:12:42 -0700

    forms.py add tuplefy functions and SelectMetro class (not fully functional)

--- steves_twitter_apps/geohash/forms.py
+++ steves_twitter_apps/geohash/forms.py
@@ -1,27 +1,35 @@
 from django import forms
 from .models import Woeids
 
-q_set = Woeids.objects.values('country').distinct().order_by('country')
-COUNTRIES = []
-for d in q_set:
-    for k, v in d.items():
-        COUNTRIES.append((v,v))
 
-"""
-FRUIT_CHOICES= [
-    ('orange', 'Oranges'),
-    ('cantaloupe', 'Cantaloupes'),
-    ('mango', 'Mangoes'),
-    ('honeydew', 'Honeydews'),
-    ]
+def tuplefy_countries():
+    """Query Woeids model for sorted list of countries and reformat into list of tuples
+    for consumption by SelectCountry form class.
+    """
+    q_set = Woeids.objects.values('country').distinct().order_by('country')
+    COUNTRIES = []
+    for d in q_set:
+        for k, v in d.items():
+            COUNTRIES.append((v,v))
+    return COUNTRIES
 
-class UserForm(forms.Form):
-    first_name= forms.CharField(max_length=100)
-    last_name= forms.CharField(max_length=100)
-    email= forms.EmailField()
-    age= forms.IntegerField()
-    favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.Select(choices=FRUIT_CHOICES))
-"""
+def tuplefy_names(country='United States'):
+    """Query Woeids model for sorted list of 'names', i.e., metro areas, within the 
+    selected country.
+    """
+    NAMES = []
+    q_set = Woeids.objects.filter(country=country).values('name').order_by('name')
+    for d in q_set:
+        for k, v in d.items():
+            NAMES.append((v,v,))
+    return NAMES
+
 
 class SelectCountry(forms.Form):
-    country = forms.ChoiceField(widget=forms.Select, choices=COUNTRIES)
+    country = forms.ChoiceField(widget=forms.Select, choices=tuplefy_countries())
+
+class SelectMetro(forms.Form):
+    def __init__(self, country='United States'):
+        self.country = country
+
+    metro = forms.ChoiceField(widget=forms.Select, choices=tuplefy_names())
