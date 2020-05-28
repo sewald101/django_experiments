7da094e743f6f8bcf6c2c22c725384db82f832b4
sewald101 <sewald101@gmail.com>
2020-05-28 07:41:23 -0700

    Completed first UI for metro selection, but unresolved BUG: still displaying results when none exist for woeids that are not GEO categories

--- steves_twitter_apps/geohash/views.py
+++ steves_twitter_apps/geohash/views.py
@@ -1,3 +1,7 @@
+import json
+import numpy as np
+import pandas as pd
+import os
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
 from django.template import loader
@@ -5,22 +9,11 @@ from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
-from .models import Woeids
-
-from .forms import SelectCountry
-
-import json
-import numpy as np
-import pandas as pd
-import os
-
-from .models import Woeids
 
 from .top_bygeo import trending_by_geo
+from .models import Woeids
+from .forms import SelectCountry
 
-# pick this up later
-# with open('/Users/sewald101/django_lab/twitter_experiments/steves_twitter_apps/geohash/woeids.json') as d:
-#     woeids = json.load(d)
 
 # Create your views here.
 def index(request):
@@ -32,14 +25,14 @@ def index(request):
 
 
 def tophash_by_country(request):
-    """Return results from country entered via the dropdown form on the homepage.
+    """Return results from country entered via the dropdown form on homepage.
     """
     print("LOG 2.0 - request object: {}".format(request))
     print("LOG 2.1 - request.GET object: {}".format(request.GET))
     print("LOG 2.2 - request.GET.country object: {}".format(request.GET['country']))
-    country = request.GET['country'].title()
+    country = request.GET['country']
     querySet = (
-        Woeids.objects.filter(name = country).filter(country = country).values('woeid')
+Woeids.objects.filter(name = country).filter(country = country).values('woeid')
         )
     print("LOG 2.3 - This is the country being passed to the Woeids query: {}"
           .format(country))
@@ -49,13 +42,16 @@ def tophash_by_country(request):
     top_hashes = trending_by_geo(woeid=woeid)
     
     _NAMES = []
-    q_set = Woeids.objects.filter(country=country).values('name').order_by('name')
+    q_set = (
+        Woeids.objects.filter(country=country).values('name').order_by('name')
+    )
     for d in q_set:
         for k, v in d.items():
-           _NAMES.append((v,v,))
+           _NAMES.append((v,v))
+    _NAMES.insert(0, ('', ''))
     
-    if len(_NAMES) > 1: # Test if metros under countries; _NAMES will always incl
-                        # country name (hence, test > 1)
+    if len(_NAMES) > 1: # Test if metros under countries; _NAMES will always
+                        # include country name (hence, test > 1)
         print("LOG 2.5 - Metro Names: {}".format(_NAMES))
         class _SelectMetro(forms.Form):
             metro = forms.ChoiceField(widget=forms.Select, choices=_NAMES)
@@ -68,8 +64,19 @@ def tophash_by_country(request):
 
 
 def tophash_by_metro(request):
-    pass
-
+    """Return result from metro entered via dropdown from country results page.
+    """
+    metro = request.GET['metro']
+    querySet = (
+        Woeids.objects.filter(name = metro).values('woeid', 'country')
+    )
+    woeid = querySet[0]['woeid']
+    print("LOG 3.1 - woeid: {}".format(woeid))
+    country = querySet[0]['country']
+    top_hashes = trending_by_geo(woeid=woeid)
+    print("LOG 3.2 - top_hashes output: {}".format(top_hashes))
+    context = {'name': metro, 'country': country, 'd': top_hashes}
+    return render(request, "geohash/result.html", context)
 
 
 def tophash_manual(request, name='', country=''):
@@ -87,7 +94,7 @@ def tophash_manual(request, name='', country=''):
         )
     elif not name: # For countries at country grain
         querySet = (
-        Woeids.objects.filter(name = country).filter(country = country).values('woeid')
+Woeids.objects.filter(name = country).filter(country = country).values('woeid')
         )
     else: # For country, city
         querySet = (
